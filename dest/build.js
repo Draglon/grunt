cf7e0ce5b01dce24026ad86da5b531b3684882ac
serg <s.nazarchuck@gmail.com>
2016-07-15 16:55:25 +0300

    add dest/

---
+++ dest/build.js
@@ -0,0 +1,16 @@
+/* test-grunt-project -v0.0.1 - <%= grunt.template.today("yyyy-mm-dd") */
+(function () {
+    var one;
+    var funcOne = function () {
+        one = 1;
+        console.log(one);
+        one++;
+    };
+})();
+(function () {
+    var two;
+    var funcTwo = function () {
+        two = 2;
+        console.log(two);
+    };
+})();
